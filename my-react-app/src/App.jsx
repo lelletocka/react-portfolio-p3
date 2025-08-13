cb1a8b7f361b3cf22a06cee52adff1b18d19b8b1
lelletocka <linnea.valtersson@hotmail.se>
2025-08-13 14:47:57 +0200

    create components and add projects and images

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,10 +1,43 @@
 import Project from "./components/Project.jsx";
+import DuckFormImage from "./images/duck-form-project.png";
+import AnimationImage from "./images/animation-project.png";
+import BookTrackerImage from "./images/book-tracker-project.png";
+
 
 function App() {
 
   return (
     <>
-      <Project />
+      <Project 
+        projectName="Duck Form"
+        img={DuckFormImage} 
+        tech="Html, CSS" 
+        githubLink="https://github.com/lelletocka/duck-form"
+        answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        answerRole="Lorem"
+        answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+      <Project 
+        projectName=""
+        img={AnimationImage}
+        tech="Html, CSS"
+        githubLink="https://github.com/lelletocka/animation-svg"
+        answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        answerRole="Lorem"
+        answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
+      <Project 
+        projectName="Book-tracker"
+        img={BookTrackerImage}
+        tech="JavaScript"
+        githubLink="https://github.com/lelletocka/js_06"
+        answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
+        answerRole="Lorem"
+        answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+        answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+      />
     </>
   ) 
   
--- my-react-app/src/components/Project.jsx
+++ my-react-app/src/components/Project.jsx
@@ -1,23 +1,25 @@
-
+import "../Project.css";
 
 function Project(props) {
 
     return (
-        <div>
-            <h2>{props.projectName}</h2>
-            <img className="screenshot" src={props.img} alt="Screenshot of project" />
-            <p>Tech: {props.tech}</p>
-            <p>Github repo: <a className="github-link" href={props.githubLink}>{props.githubLink}</a></p>           
-            <div>
-                <h3>About the project</h3>
+        <div className="project-div">
+            <div className="title-div">
+                <h2>{props.projectName}</h2>
+                <p className="title-p tech-p">{props.tech}</p>
+                <p className="title-p github-p"><a className="github-link" href={props.githubLink}>{props.githubLink}</a></p>
+            </div>
+            <img className="project-img" src={props.img} alt={"Screenshot of the " + props.projectName + " project"} />                       
+            <div className="about-div">
+                <h3 className="about-h">About the project</h3>
                 <h4 className="subject_h">The Project</h4>
-                <p className="subject_p">{props.answerOne}</p>
+                <p className="subject_p">{props.answerProject}</p>
                 <h4 className="subject_h">Role</h4>
-                <p className="subject_p">{props.answerTwo}</p>
+                <p className="subject_p">{props.answerRole}</p>
                 <h4 className="subject_h">Challenges</h4>
-                <p className="subject_p">{props.answerThree}</p>
+                <p className="subject_p">{props.answerChallenges}</p>
                 <h4 className="subject_h">What I Learned</h4>
-                <p className="subject_p">{props.answerFour}</p>
+                <p className="subject_p">{props.answerLearned}</p>
             </div>
 
 
