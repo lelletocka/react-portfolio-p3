5352464aa3bce9e78b87bea6e1047e2a57d63961
lelletocka <linnea.valtersson@hotmail.se>
2025-08-18 13:18:01 +0200

    move projects content to array

--- my-react-app/src/pages/Portfolio.jsx
+++ my-react-app/src/pages/Portfolio.jsx
@@ -38,50 +38,58 @@ function Portfolio() {
             setIsPopThreeOpen(false);
         };
 
-          
+        const projects = [
+            {
+                projectName: "Duck Form",
+                img: {DuckFormImage},
+                tech: "Html, CSS",
+                githubLink: "https://github.com/lelletocka/duck-form",
+                answerProject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                answerRole: "Lorem",
+                answerChallenges: " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisiut aliquip ex ea commodo consequat.",
+                answerLearned: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            },
+            {
+                projectName: "Animation and SVG",
+                img: {AnimationImage},
+                tech: "Html, CSS",
+                githubLink: "https://github.com/lelletocka/animation-svg",
+                answerProject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                answerRole: "Lorem",
+                answerChallenges: " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                answerLearned: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",                    
+            },
+            {
+                projectName: "Book-tracker",
+                img: {BookTrackerImage},
+                tech: "JavaScript",
+                githubLink: "https://github.com/lelletocka/js_06",
+                answerProject: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua",
+                answerRole: "Lorem",
+                answerChallenges: " Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+                answerLearned: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+            },
+        ];  
         
         return (
             <div>
-                <div onClick={() => setIsPopOneOpen(true)}>
-                    <Project 
-  
-                    projectName="Duck Form"
-                    img={DuckFormImage} 
-                    tech="Html, CSS" 
-                    githubLink="https://github.com/lelletocka/duck-form"
-                    answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-                    answerRole="Lorem"
-                    answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    />
+                {projects.map((project, index) => (
+                    <div key={index} >
+                        <Project
+                            projectName={project.projectName}
+                            img={project.img} 
+                            tech={project.tech} 
+                            githubLink={project.githubLink}
+                            answerProject={project.answerProject}
+                            answerRole={project.answerRole}
+                            answerChallenges={project.answerChallenges}
+                            answerLearned={project.answerLearned}
+                        />
+                    </div>
+                ))}
 
-                </div>
-                <div onClick={() => setIsPopTwoOpen(true)}>
-                    <Project 
-                    
-                    projectName="Animation and SVG"
-                    img={AnimationImage}
-                    tech="Html, CSS"
-                    githubLink="https://github.com/lelletocka/animation-svg"
-                    answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-                    answerRole="Lorem"
-                    answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    />
-                </div>
-                <div onClick={() => setIsPopThreeOpen(true)}>
-                    <Project 
-                    
-                    projectName="Book-tracker"
-                    img={BookTrackerImage}
-                    tech="JavaScript"
-                    githubLink="https://github.com/lelletocka/js_06"
-                    answerProject="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua"
-                    answerRole="Lorem"
-                    answerChallenges=" Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-                    answerLearned="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-                    />
-                </div>
+
+               
                 
                 {isPopOneOpen && (
                    <PopupProject 
